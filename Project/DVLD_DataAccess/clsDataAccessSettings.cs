d3732458f3d1d021e98992c0ca26a78a6d2e978a
salah <sm8109256@gmail.com>
2025-10-18 15:53:10 +0300

    Add DVLD project as part of main repo

---
+++ Project/DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+    }
+}
